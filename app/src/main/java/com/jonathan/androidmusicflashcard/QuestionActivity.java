71877ec556ef2cb953bda13bde60334d2fe3d3b1
Guillaume <guillaume.pinheiro@edu.itescia>
2021-02-24 10:45:39 +0100

    Story 1
    + added Material Design (not in use for now) + music note png + first debug mp3

---
+++ app/src/main/java/com/jonathan/androidmusicflashcard/QuestionActivity.java
@@ -0,0 +1,17 @@
+package com.jonathan.androidmusicflashcard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.RadioGroup;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
