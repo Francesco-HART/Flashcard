2106925f48a1c8c14f17bea098c5f9f73a5adfb4
Guillaume <guillaume.pinheiro@edu.itescia>
2021-02-24 12:01:42 +0100

    Story 3
    Dynamically generated radio radio buttons from shuffled answer list

--- app/src/main/java/com/jonathan/androidmusicflashcard/QuestionActivity.java
+++ app/src/main/java/com/jonathan/androidmusicflashcard/QuestionActivity.java
@@ -10,6 +10,10 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class QuestionActivity extends AppCompatActivity {
 
     @Override
@@ -17,10 +21,25 @@ public class QuestionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
 
+        RadioGroup radioGroup = findViewById(R.id.radioGroup1);
+
+        List<String> answersList = new ArrayList<String>();
+        answersList.add("Answer 1");
+        answersList.add("Answer 2");
+        answersList.add("Answer 3");
+
+        Collections.shuffle(answersList);
+
+        final RadioButton[] generatedRadioButtons = new RadioButton[answersList.size()];
+        for(int i=0; i<answersList.size(); i++){
+            generatedRadioButtons[i]  = new RadioButton(this);
+            radioGroup.addView(generatedRadioButtons[i]);
+            generatedRadioButtons[i].setText(answersList.get(i));
+        }
+
         Button confirmButton = findViewById(R.id.confirmButton);
 
         confirmButton.setOnClickListener(v -> {
-            RadioGroup radioGroup = findViewById(R.id.radioGroup1);
             RadioButton checkedRadioButton = findViewById(radioGroup.getCheckedRadioButtonId());
 
             String userAnswer = checkedRadioButton.getText().toString();
