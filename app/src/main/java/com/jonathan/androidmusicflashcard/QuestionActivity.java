3888fe084bb6d1c3c4ad61b3e046c70360554a26
Guillaume <guillaume.pinheiro@edu.itescia>
2021-02-24 11:18:35 +0100

    Story 2
    Checked radio button detection + answer comparison + display correct/wrong answer to the user

--- app/src/main/java/com/jonathan/androidmusicflashcard/QuestionActivity.java
+++ app/src/main/java/com/jonathan/androidmusicflashcard/QuestionActivity.java
@@ -2,10 +2,13 @@ package com.jonathan.androidmusicflashcard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class QuestionActivity extends AppCompatActivity {
 
@@ -13,5 +16,28 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        Button confirmButton = findViewById(R.id.confirmButton);
+
+        confirmButton.setOnClickListener(v -> {
+            RadioGroup radioGroup = findViewById(R.id.radioGroup1);
+            RadioButton checkedRadioButton = findViewById(radioGroup.getCheckedRadioButtonId());
+
+            String userAnswer = checkedRadioButton.getText().toString();
+            String correctAnswer = "Answer 1";
+
+            TextView resultTextView = findViewById(R.id.resultTextView);
+
+            if (userAnswer.equals(correctAnswer))
+            {
+                resultTextView.setTextColor(Color.GREEN);
+                resultTextView.setText("Correct answer");
+            }
+            else
+            {
+                resultTextView.setTextColor(Color.RED);
+                resultTextView.setText("Wrong answer. The right answer was " + correctAnswer);
+            }
+        });
     }
 }
